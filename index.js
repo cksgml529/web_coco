2670e5afae85c14d1bc6812a9d2e0a843259d9ee
cksgml529 <cherryb0409@naver.com>
2023-03-01 23:57:47 +0900

    로그인 페이지 추가

---
+++ index.js
@@ -0,0 +1,27 @@
+// topBtn
+const topBtn = document.querySelector(".top_btn");
+topBtn.addEventListener("click", () => {
+  scrollTo({
+    top:0,
+    behavior:"smooth"
+  });
+  });
+//   menuList
+const menu = document.querySelectorAll(".sub_menu>li");
+menu.forEach((item)=>{
+    item.addEventListener("click",()=> alert('현재 작업중입니다.'))
+    })
+
+// productList
+const product = document.querySelectorAll(".product_list>li");
+product.forEach((item)=>item.addEventListener("click",()=>alert("현재 작업중입니다.")))
+
+// scroll 이벤트
+const section = document.querySelectorAll("section");
+window.addEventListener('scroll',()=>{
+    section.forEach(item => {
+        if(window.scrollY>=item.offsetTop-500){
+            item.classList.add('active');
+        }
+    });
+})
---
+++ layout/login.js
@@ -0,0 +1,8 @@
+const userID = document.querySelector("#userID");
+const userPW = document.querySelector("#userPW");
+const search = document.querySelectorAll(".none");
+
+search.forEach((item) => {
+    item.addEventListener("click",()=>alert('현재 작업중입니다.'));
+});
+
